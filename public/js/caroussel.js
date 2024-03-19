dd8bc5e7bcf1bf49f39c95a561cb5644753dc9fa
Thibault Paul <xpaul.thibault@gmail.com>
2024-03-19 23:19:04 +0100

    Update dependencies and fix caroussel.js

--- public/js/caroussel.js
+++ public/js/caroussel.js
@@ -1,14 +1,10 @@
-import Swiper from "/node_modules/swiper/swiper-bundle.min.mjs";
-
 document.addEventListener("DOMContentLoaded", function () {
-  // Initialiser Swiper
   var mySwiper = new Swiper(".swiper-container", {
-    // Paramètres du carrousel
-    slidesPerView: 1, // Nombre de slides visibles à la fois
-    spaceBetween: 30, // Espace entre les slides
-    loop: true, // Activer le mode boucle
+    slidesPerView: 1,
+    loop: false,
     autoplay: {
-      delay: 2000, // Délai de 2 secondes entre chaque slide
+      delay: 3000,
     },
+    preloadImages: true,
   });
 });
